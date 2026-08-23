5f8ac1b7460c196d0fedc5efaee4a7ad51159f7e
Yungki <yungki1612@gmail.com>
2026-08-23 18:35:52 +0900

    fix: remove toss-theme.css import to restore dark glassmorphism theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '../style.css';
-import '../toss-theme.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
